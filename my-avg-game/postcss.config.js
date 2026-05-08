a0ba1a807c227208db88855f49960020e77634d2
jfldja <coco930228@gmail.com>
2026-05-09 06:31:14 +0800

    feat: integrate Tailwind CSS and update PostCSS configuration

--- my-avg-game/postcss.config.js
+++ my-avg-game/postcss.config.js
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // 這裡必須改為 @tailwindcss/postcss
     autoprefixer: {},
   },
 }
--- my-avg-game/src/main.tsx
+++ my-avg-game/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// src/main.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css' // 確保這一行存在，Tailwind 才會生效
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
 )
